b85faec5690565a5624751321015d8e9414564da
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-09-14 09:31:00 +0700

    set up the testing file

--- javascript-track/master.test.js
+++ javascript-track/master.test.js
@@ -1,7 +1,11 @@
-import { hello } from "./src/helloWorld";
+const helloWorldConsole = require('./src/helloWorld');
+const helloWorld = require('./src/helloWorld');
 
 describe('Hello World', () => {
-  test('Say Hi!', () => {
-    expect(hello()).toEqual('Hello, World!');
+  test('Say Hi! in Console', () => {
+    expect(helloWorld()).toEqual(console.log('Hello, World!'));
   });
+  // test('Say Hello! in return', () => {
+  //   expect(helloWorld()).toEqual("Hello, World!");
+  // })
 });
