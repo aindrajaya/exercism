f9b445a052c8776f7c80510f6feab942ddf56e33
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-09-14 08:41:00 +0700

    set module exports

--- javascript-track/src/helloWorld/index.js
+++ javascript-track/src/helloWorld/index.js
@@ -1,3 +1,10 @@
-export function hello(){
-  console.log("Hellssss")
+function helloWorldConsole(){
+  console.log("Hello, World!");
 }
+
+function helloWorld(){
+  return "Hello, World!";
+}
+
+// module.exports = helloWorld
+module.exports = helloWorldConsole
